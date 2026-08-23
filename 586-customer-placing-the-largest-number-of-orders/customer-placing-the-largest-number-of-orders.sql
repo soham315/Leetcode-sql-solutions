f8f8e036d35235dbd26508bc1308a4eb9b1654ea
Soham Chakraborty <72433064+soham315@users.noreply.github.com>
2026-08-23 13:37:05 +0530

    Time: 493 ms (61.32%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,8 +1,11 @@
-WITH cte AS
-(SELECT customer_number, COUNT(order_number) AS NumOrd
-FROM Orders
-GROUP BY customer_number)
+-- With CTE as(select customer_number , count(order_number) as order_count
+-- from orders
+-- group by customer_number)
 
-SELECT customer_number
-FROM cte
-WHERE NumOrd = (SELECT Max(NumOrd) FROM cte)
+-- select customer_number from CTE where order_count = (select max(order_count) from cte)
+
+with cte as(select customer_number,count(order_number) as count_order
+from orders
+group by customer_number)
+
+select customer_number from cte where count_order = (select max(count_order) from cte)
